ec36e5554458ced4cd3fa4054d5ba787cf9a43e7
shu <mrkmsm1009@gmail.com>
2024-02-20 22:34:06 +0900

    first commit

---
+++ CardRegistration/CardRegistration/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace CardRegistration
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# 電子錠
+
+## CardRegistration
+学生証の登録、電子錠に送信するwindowsソフト  
+
+## LockBody
+電子錠内部プログラム
